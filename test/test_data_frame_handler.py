2324a546b8e4e088daa329261d6b78792d930229
Felipe Pedrosa <marconizero@gmail.com>
2022-10-05 15:50:27 -0300

    test: ensure that calls drop_null_lines method with correct value

--- test/test_data_frame_handler.py
+++ test/test_data_frame_handler.py
@@ -11,6 +11,9 @@ class TestClassDataFrameHandler:
         def get_data_frame(self, path: str) -> object:
             return object
 
+        def drop_null_lines(self, data_frame: object) -> object:
+            return object
+
     CSV_ANALYSER_STUB: CsvAnalyserInterface = CsvAnalyserStub()
     SUT: DataFrameHandler = DataFrameHandler(CSV_ANALYSER_STUB)
 
@@ -28,3 +31,18 @@ class TestClassDataFrameHandler:
         CSV_PATH = 'any_path'
         SUT.handler(CSV_PATH)
         SPY.assert_called_once_with(CSV_PATH)
+
+    @pytest.mark.parametrize(
+        "SUT, CSV_ANALYSER_STUB",
+        [(SUT, CSV_ANALYSER_STUB)]
+    )
+    def test_should_call_drop_null_lines_with_correct_value(
+        self,
+        SUT: DataFrameHandler,
+        CSV_ANALYSER_STUB: CsvAnalyserInterface,
+        mocker: MockerFixture
+    ):
+        SPY = mocker.spy(CSV_ANALYSER_STUB, 'drop_null_lines')
+        CSV_PATH = 'any_path'
+        SUT.handler(CSV_PATH)
+        SPY.assert_called_once_with(object)
