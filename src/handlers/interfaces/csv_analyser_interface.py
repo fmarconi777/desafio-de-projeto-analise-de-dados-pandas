052d0c8d47286a2933d8d33703a214b5662f41a7
Felipe Pedrosa <marconizero@gmail.com>
2022-10-07 11:04:35 -0300

    refactor: change object type for Any

--- src/handlers/interfaces/csv_analyser_interface.py
+++ src/handlers/interfaces/csv_analyser_interface.py
@@ -1,24 +1,25 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class CsvAnalyserInterface(ABC):
 
     @abstractmethod
-    def get_data_frame(self, path: str) -> object:
+    def get_data_frame(self, path: str) -> Any:
         pass
 
     @abstractmethod
-    def drop_null_lines(self, data_frame: object) -> object:
+    def drop_null_lines(self, data_frame: Any) -> Any:
         pass
 
     @abstractmethod
-    def rename_columns(self, data_frame: object, renamed_columns: dict) -> object:
+    def rename_columns(self, data_frame: Any, renamed_columns: dict) -> Any:
         pass
 
     @abstractmethod
-    def groupby_sum_plot(self, data_frame: object, groupby_plot: list[str], path: str) -> None:
+    def groupby_sum_plot(self, data_frame: Any, groupby_plot: list[str], path: str) -> None:
         pass
 
     @abstractmethod
-    def groupby_mean_plot(self, data_frame: object, groupby_plot: list[str], path: str) -> None:
+    def groupby_mean_plot(self, data_frame: Any, groupby_plot: list[str], path: str) -> None:
         pass
